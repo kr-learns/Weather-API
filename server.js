6eb61e83cbe181d257d72e6ec0a1b188723b8675
Aditya <adityasahoo652@gmail.com>
2025-03-16 11:03:39 +0530

    Refactor server initialization to export app and server instances

--- server.js
+++ server.js
@@ -136,6 +136,8 @@ app.get("/api/weather/:city", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = { app, server };
