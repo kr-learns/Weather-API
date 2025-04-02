126faeb83b38055ed47d9ac19f93b7d153ccd49d
Gaurav Karakoti <karakotigaurav12@gmail.com>
2025-04-03 00:07:23 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -199,7 +199,7 @@ function isLocalStorageAvailable() {
 
 async function loadConfig() {
     try {
-        const response = await fetch('https://weather-api-ex1z.onrender.com/config');
+        const response = await fetch('/config');
         if (!response.ok) throw new Error('Failed to load config');
 
         const config = await response.json();
